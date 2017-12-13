d381b1add22ef8e70ec4f31728130cdf4b47438b
RHuff1 <RHuff1@users.noreply.github.com>
2017-12-13 10:21:08 -0500

    Final Changes

--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@ using namespace std;
 
 int currentTemp = 0;
 int targetTemp = 75;
-int threshold = 1;
+int threshold = 2;
+bool modeChange = false;
 
 Motor m(p23, p5, p6); // pwm, fwd, rev - top motor
 Motor m2(p24, p9, p8); // pwm, fwd, rev - bottom motor
@@ -21,6 +22,7 @@ DigitalIn pb2(p21); // Decrease temp
 
 enum Modetype { MODE_OFF = 0, MODE_ON };
 Modetype mode = MODE_OFF;
+Modetype previousMode = MODE_OFF;
 enum Statetype { HEAT_OFF = 0, HEAT_ON, COOL_OFF, COOL_ON };
 Statetype state = HEAT_OFF;
 
@@ -70,8 +72,11 @@ void messageArrived3(MQTT::MessageData& md) // control: On or Off
     // can't just use (char*)message.payload, some weird stuff gets printed at the end
     char buf[5];
     sprintf(buf, "%.*s", message.payloadlen, (char*)message.payload);
-
+    previousMode = mode;
     mode = (Modetype)atoi(buf);
+    if (mode != previousMode) {
+        modeChange = true;
+    }
 }
 
 void controlHeat()  // move knob to heat
@@ -122,37 +127,12 @@ void controlOff()  // turn switch off
     m2.speed(0);
 }
 
-//void pb1_hit_callback (void)
-//{
-//    char buf[100];
-//    sprintf(buf, "%d", currentTemp + 1);
-//    messagePb.payload = (void*)buf;
-//    messagePb.payloadlen = strlen(buf)+1;
-//    pbFlag = true;
-//    printf("Increase temp flag set.\r\n");
-//    //client.publish("temperature", messagePb);
-//}
-
-//void pb2_hit_callback (void)
-//{
-//    char buf[100];
-//    sprintf(buf, "%d", currentTemp - 1);
-//    messagePb.payload = (void*)buf;
-//    messagePb.payloadlen = strlen(buf)+1;
-//    pbFlag = true;
-//    printf("Decrease temp flag set.\r\n");
-//    //client.publish("temperature", messagePb);
-//}
 
 int main()
 {
     pb1.mode(PullUp);
     pb2.mode(PullUp);
     wait(.001);
-//    pb1.attach_deasserted(&pb1_hit_callback);
-//    pb2.attach_deasserted(&pb2_hit_callback);
-//    pb1.setSampleFrequency();
-//    pb2.setSampleFrequency();
 
     uLCD.baudrate(BAUD_3000000);
     uLCD.cls();
@@ -192,7 +172,7 @@ int main()
 
     if ((rc = client.subscribe("temperature", MQTT::QOS2, messageArrived)) != 0)
         printf("rc from MQTT subscribe is %d\r\n", rc);
-    if ((rc = client.subscribe("ideal", MQTT::QOS2, messageArrived2)) != 0)
+    if ((rc = client.subscribe("finalTargetTemp", MQTT::QOS2, messageArrived2)) != 0)
         printf("rc from MQTT subscribe is %d\r\n", rc);
     if ((rc = client.subscribe("control", MQTT::QOS2, messageArrived3)) != 0)
         printf("rc from MQTT subscribe is %d\r\n", rc);
@@ -205,32 +185,23 @@ int main()
     while(true) {
         //printf("start yield\r\n");
         client.yield(100);
-        //printf("end yield\r\n");
-        // if(client.yield(100) == -1){ // disconnected, try reconnecting
-        //  if ((rc = client.connect(data)) != 0)
-        //      printf("rc from MQTT connect is %d\r\n", rc);
-        //  if ((rc = client.subscribe("temperature", MQTT::QOS2, messageArrived)) != 0)
-        //      printf("rc from MQTT subscribe is %d\r\n", rc);
-        //  if ((rc = client.subscribe("ideal", MQTT::QOS2, messageArrived2)) != 0)
-        //      printf("rc from MQTT subscribe is %d\r\n", rc);
-        // }
 
         if (pb1 == 0) {
             char buf[100];
-            sprintf(buf, "%d", currentTemp + 1);
+            sprintf(buf, "%d", targetTemp + 1);
             messagePb.payload = (void*)buf;
             messagePb.payloadlen = strlen(buf)+1;
             printf("Increase temp flag set.\r\n");
-            client.publish("temperature", messagePb);
+            client.publish("ideal", messagePb);
             wait(.5);
         }
         if (pb2 == 0) {
             char buf[100];
-            sprintf(buf, "%d", currentTemp - 1);
+            sprintf(buf, "%d", targetTemp - 1);
             messagePb.payload = (void*)buf;
             messagePb.payloadlen = strlen(buf)+1;
             printf("Decrease temp flag set.\r\n");
-            client.publish("temperature", messagePb);
+            client.publish("ideal", messagePb);
             wait(.5);
         }
 
@@ -238,6 +209,10 @@ int main()
         printf("State: %d\r\n", state);
 
         if(mode == MODE_OFF) { // don't do anything
+            if (modeChange) {
+                controlOff();
+                modeChange = false;
+            }
             continue;
         }
 
